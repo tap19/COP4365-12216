dde3d1cb0bd21b4c3a1eeaf9ea115ff91117cdb3
Patrick S Taylor <patrick@pstaylor.net>
2017-04-05 01:08:15 -0500

    added documentation to BubbleSort_02 & BubbleSort_03 to further clarify what each should solve

--- COP4365-12216/Project3/BubbleSort_02.cs
+++ COP4365-12216/Project3/BubbleSort_02.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Project3
 {
+    /// <summary>
+    /// implementation of Example 18.6 a using overridden BubbleSort.Sort() method
+    /// </summary>
     class BubbleSort_02 : BubbleSort
     {
         public BubbleSort_02()
         {
 
         }
+        /*
+         * After the first pass, the largest number is guaranteed to be in the highest-numbered element 
+         * of the array(for an array being sorted in ascending order); after the second pass,
+         * the two highest numbers are “in place”; and so on.Instead of making nine comparisons
+         * on every pass of a 10-element array, modify the bubble sort to make eight comparisons
+         * on the second pass, seven on the third pass and so on.
+         */
     }
 }
--- COP4365-12216/Project3/BubbleSort_03.cs
+++ COP4365-12216/Project3/BubbleSort_03.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Project3
 {
+    /// <summary>
+    /// implementation of Example 18.6 a using overridden BubbleSort.Sort() method
+    /// </summary>
     class BubbleSort_03 : BubbleSort
     {
         public BubbleSort_03()
         {
 
         }
+        /*
+         *The data in the array may already be in the proper order or near-proper order, so why
+         * make nine passes of a 10-element array if fewer will suffice? Modify the sort to check at
+         * the end of each pass whether any swaps have been made.If none have been made, the
+         * data must already be in the proper order, so the app should terminate.If swaps have
+         * been made, at least one more pass is needed.
+         */
     }
 }
